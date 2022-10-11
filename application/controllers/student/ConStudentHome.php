0a385ceb422d276f96d9bcadb8126660e418c71d
dekpiano <dekpiano4815@gmail.com>
2022-10-11 09:15:45 +0700

    11/10/65 อัพเดพดูเกรดนักเรียน เรียบร้อย

--- application/controllers/student/ConStudentHome.php
+++ application/controllers/student/ConStudentHome.php
@@ -13,6 +13,26 @@ var  $title = "ผลการเรียน";
 
     }
 
+    function getClient()
+    {
+        require_once APPPATH. '../vendor/google_sheet/vendor/autoload.php';
+    
+        // configure the Google Client
+        $client = new \Google_Client();
+        $client->setApplicationName('Google Sheets API');
+        $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+        $client->setAccessType('offline');
+        // credentials.json is the key file we downloaded while setting up our Google Sheets API
+        $path = 'service_key.json';
+        $client->setAuthConfig($path);
+
+        // configure the Sheets Service
+        $service = new \Google_Service_Sheets($client);
+         
+        return $service;
+    }
+
+
     public function Home(){
         $data['title'] = "หน้าแรก";
         $data['ExtraSetting'] = $this->db->get('tb_extra_setting')->result();
@@ -55,6 +75,39 @@ var  $title = "ผลการเรียน";
       
         $data['CheckOnOff'] = $this->db->select('*')->from('tb_register_onoff')->get()->result();
         //echo '<pre>'; print_r($this->session->userdata('login_id')); exit();
+        $data['stu'] =  $this->db->select('
+                                    StudentClass,
+                                    StudentCode,
+                                    StudentPrefix,
+                                    StudentFirstName,
+                                    StudentLastName 
+                                    ')
+                                 ->where('StudentID',$this->session->userdata('login_id'))->get('tb_students')->row();
+
+        $this->load->helper('array');
+        $service = $this->getClient();
+        $spreadsheetId = '1eMgeASo3Vqxh8O0pERAJ0WO_9MLVx4wkuiJEFjquAfQ';
+        
+        $range_checkChunum = 'ชุมนุม!A3:F1000';  // TODO: Update placeholder value.
+        $response_checkChunum = $service->spreadsheets_values->get($spreadsheetId, $range_checkChunum);
+        $numRows_checkChunum = $response_checkChunum->getValues() != null ? count($response_checkChunum->getValues()) : 0;
+       
+        $range_ruksun = 'ลูกเสือ!A3:F1000';  // TODO: Update placeholder value.
+        $response_ruksun = $service->spreadsheets_values->get($spreadsheetId, $range_ruksun);
+        $numRows_ruksun = $response_ruksun->getValues() != null ? count($response_ruksun->getValues()) : 0;
+      
+       $checkChunum = [];
+       foreach ($response_checkChunum->values as $key => $value) {
+        $checkChunum[] = $value[1];
+       }   
+       $data['checkChunum']  = $checkChunum;
+     
+       $checkRuksun = [];
+       foreach ($response_ruksun->values as $key => $value) {
+        $checkRuksun[] = $value[1];
+       }   
+       $data['checkRuksun']  = $checkRuksun;
+
 
         $this->load->view('student/layout/HeaderStudent.php',$data);
         $this->load->view('student/PageAcademicResult.php');
