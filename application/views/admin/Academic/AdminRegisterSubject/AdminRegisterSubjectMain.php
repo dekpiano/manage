5235cea094a441fc7228ed5d5bcddc9308c0b7aa
dekpiano <dekpiano4815@gmail.com>
2025-06-04 10:14:54 +0700

    4/6/2568 เพิ่ม หน่วยกิต

--- application/views/admin/Academic/AdminRegisterSubject/AdminRegisterSubjectMain.php
+++ application/views/admin/Academic/AdminRegisterSubject/AdminRegisterSubjectMain.php
@@ -119,7 +119,7 @@
                                             </label>
                                             <select class="form-select" required="" name="SubjectUnit" id="SubjectUnit">
                                                 <option value="">เลือกหน่วยกิต</option>
-                                                <?php $Unit = array("0.5","1.0","1.5","2.0");
+                                                <?php $Unit = array("0.5","1.0","1.5","2.0","2.5","3.0","3.5","4.0","4.5","5.0");
                                                 foreach ($Unit as $key => $v_Unit):?>
                                                 <option value="<?=$v_Unit?>"><?=$v_Unit?></option>
                                                 <?php endforeach; ?>
